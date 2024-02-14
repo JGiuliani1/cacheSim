b488f492e13bf055a01fb2fc11720b2e5e13b51d
JGiuliani1 <112366171+JGiuliani1@users.noreply.github.com>
2024-02-14 14:42:20 -0500

    Add files via upload

---
+++ cachesim.py
@@ -0,0 +1,22 @@
+class cache:
+    pass
+
+class cacheSet:
+    pass
+
+class cacheBlock:
+    def __init__(self, tag, clean):
+        self.tag = tag
+        self.valid = False
+        self.clean = clean
+
+def main():
+    pass
+
+def readWord(adderess):
+    pass
+
+def writeWord(address, word):
+    pass
+
+main()
