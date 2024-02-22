057d961cfa9fc652ef687e9e766e26184cc9fa72
JGiuliani1 <112366171+JGiuliani1@users.noreply.github.com>
2024-02-21 21:23:16 -0500

    Add files via upload

--- cachesim.py
+++ cachesim.py
@@ -6,6 +6,7 @@ ASSOCIATIVITY = 1
 NUM_SETS = CACHE_SIZE // CACHE_BLOCK_SIZE
 
 
+# helper funtions
 def logb2(val):
         i = 0
         assert val > 0
@@ -23,16 +24,18 @@ class CacheBlock:
         self.valid = False
         self.data = bytearray(cache_block_size)
 
+
 class CacheSet:
     def __init__(self, cache_block_size, associativity):
         self.blocks = [CacheBlock(cache_block_size) for i in range(associativity)]
         self.tag_queue = [-1 for i in range(associativity)] # not needed for Part One
 
+
 class Cache:
     def __init__(self, num_sets, associativity, cache_block_size):
         self.write_through = False # not needed for Part One
         self.sets = [CacheSet(cache_block_size, associativity) for i in range(num_sets)]
-        memory_size_bits = logb2(MEM_SIZE)
+        self.memory_size_bits = logb2(MEM_SIZE)
         self.cache_size_bits = logb2(CACHE_SIZE)
         self.cache_block_size_bits = logb2(CACHE_BLOCK_SIZE)
         self.index_length = logb2(num_sets)
@@ -43,6 +46,7 @@ class Cache:
 sysCache = Cache(NUM_SETS, ASSOCIATIVITY, CACHE_BLOCK_SIZE)
 memory = bytearray(MEM_SIZE)
 
+
 def main():
     readWord(46916)
     readWord(46932)
@@ -52,8 +56,11 @@ def main():
     readWord(56132)
 
 
+#TODO fix return statement
 def readWord(address):
     # validate address
+    # not checkAllignment(address):
+    #    return
 
     # calculate tag, index, and offset
     indexSize = logb2(NUM_SETS)
@@ -62,31 +69,48 @@ def readWord(address):
     index = (address >> offsetSize) & (2**indexSize - 1)
     offset = address & (2**offsetSize - 1)
 
+    # check if tag is in use in set of blocks - read hit
     for i in range(ASSOCIATIVITY):
-        if sysCache.sets[index].blocks[i].tag == tag and sysCache.sets[index].blocks[i].valid == True:
-            # cache hit
-            sysCache.sets[index].blocks[i].tag = tag
-            sysCache.sets[index].blocks[i].valid = True
+        if sysCache.sets[index].blocks[i].tag == tag:
             # update tag queue
             print("read hit: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
-        else:
-            # cache miss
+    
+    # check if there are any empty blocks in the set - read miss
+    for m in range(ASSOCIATIVITY):
+        if sysCache.sets[index].blocks[m].valid == False:
+            # update tag queue
+            sysCache.sets[index].blocks[m].tag = tag
+            sysCache.sets[index].blocks[m].valid = True
             rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
-            data = []
             for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
-                data.append(memory[j])
-            sysCache.sets[index].blocks[i].data = data
-            sysCache.sets[index].blocks[i].tag = tag
-            sysCache.sets[index].blocks[i].valid = True
-            print("read miss: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset)
+                sysCache.sets[index].blocks[m].data.append(memory[j])
+            print("read miss: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
+    
+    # replace least recently used block - read miss + replace
+    for n in range(ASSOCIATIVITY): # update for part 2
+        # find least recently used block and update tag queue
+        sysCache.sets[index].blocks[i].tag = tag
+        sysCache.sets[index].blocks[i].valid = True
+        rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
+        for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
+            sysCache.sets[index].blocks[i].data.append(memory[j])
+        print("read miss + replace: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " new range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
+        return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
+
 
 def writeWord(address, word):
     pass
 
+
 def checkAllignment(address):
-    pass
+    valid = False
+    memSize = (MEM_SIZE.bit_length() + 7) // 8
+    if memSize > address >= 0:
+        if address % 4 == 0:
+            return True
+    return False
 
 
 main()
