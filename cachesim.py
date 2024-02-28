13150aba09f7841ab9f29d4e8f456aef4b3e0428
jjklug <jackklug13@gmail.com>
2024-02-28 17:59:12 -0500

    check alignment

--- cachesim.py
+++ cachesim.py
@@ -106,9 +106,21 @@ def readWord(address):
         print(cache.sets[index].blocks[i].tag)
         if cache.sets[index].blocks[i].tag == tag:
             # update tag queue
+            # update tag queue - make sure it doesn't overfill
+            if cache.sets[index].tag_queue[0] == -1:
+                cache.sets[index].tag_queue[0] = tag
+            else:
+                for x in range(ASSOCIATIVITY - 2, -1, -1):
+                    cache.sets[index].tag_queue[x + 1] = cache.sets[index].tag_queue[x]
+                cache.sets[index].tag_queue[0] = tag
+
+            # set dirty flag to true
+            cache.sets[index].blocks[i].dirty = True
+
+            #return cache
             print("read hit: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
-    
+
     # check if there are any empty blocks in the set - read miss
     for m in range(ASSOCIATIVITY):
         if cache.sets[index].blocks[m].valid == False:
@@ -120,7 +132,7 @@ def readWord(address):
                 cache.sets[index].blocks[m].data.append(memory[j])
             print("read miss: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
-    
+
     # replace least recently used block - read miss + replace
     for n in range(ASSOCIATIVITY): # update for part 2
         # find least recently used block and update tag queue
@@ -167,6 +179,19 @@ def writeWord(address, word):
                     memory[
                         address + 3])
 
+    for m in range(ASSOCIATIVITY):
+        if cache.sets[index].blocks[m].valid == False:
+            # update tag queue
+            cache.sets[index].blocks[m].tag = tag
+            cache.sets[index].blocks[m].valid = True
+            rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
+            for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
+                cache.sets[index].blocks[m].data.append(memory[j])
+            print("read miss: address = ", address, " index: ", index, " block index: ", i, " tag: ", tag, " offset: ",
+                  offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
+            return memory[address] + 256 * memory[address + 1] + 256 ** 2 * memory[address + 2] + 256 ** 3 * memory[
+                address + 3]
+
 
 def checkAllignment(address):
     assert address + 3 < MEM_SIZE
@@ -187,6 +212,7 @@ def bytes_to_word(source, start, size):
         word = word + mult * source[start+i]
         mult = mult * 256
     return word
+
 
 
 main()
