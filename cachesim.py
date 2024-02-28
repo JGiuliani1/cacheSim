ea71f57bd23dfefe20b2662baf483f2209d8295c
JGiuliani1 <112366171+JGiuliani1@users.noreply.github.com>
2024-02-28 16:15:35 -0500

    Add files via upload

--- cachesim.py
+++ cachesim.py
@@ -1,9 +1,11 @@
-# global variables
+# global constant variables
+WORDLENGTH = 4
 MEM_SIZE = 65536
 CACHE_SIZE = 1024
 CACHE_BLOCK_SIZE = 64
 ASSOCIATIVITY = 1
-NUM_SETS = CACHE_SIZE // CACHE_BLOCK_SIZE
+NUM_SETS = CACHE_SIZE // (CACHE_BLOCK_SIZE * ASSOCIATIVITY)
+NUM_BLOCKS = CACHE_SIZE // CACHE_BLOCK_SIZE
 
 
 # helper funtions
@@ -35,7 +37,7 @@ class Cache:
     def __init__(self, num_sets, associativity, cache_block_size):
         self.write_through = False # not needed for Part One
         self.sets = [CacheSet(cache_block_size, associativity) for i in range(num_sets)]
-        self.memory_size_bits = logb2(MEM_SIZE)
+        memory_size_bits = logb2(MEM_SIZE)
         self.cache_size_bits = logb2(CACHE_SIZE)
         self.cache_block_size_bits = logb2(CACHE_BLOCK_SIZE)
         self.index_length = logb2(num_sets)
@@ -43,20 +45,18 @@ class Cache:
 
 
 # global cache and memory
-sysCache = Cache(NUM_SETS, ASSOCIATIVITY, CACHE_BLOCK_SIZE)
-list = []
-for i in range(MEM_SIZE):
-    list.append(i)
-memory = bytearray(list)
+memory = bytearray(MEM_SIZE)
+memory_size_bits = 0
+cache = None
 
 
 def main():
-    readWord(46916)
-    readWord(46932)
-    readWord(12936)
-    readWord(13964)
-    readWord(46956)
-    readWord(56132)
+    # initialize cache and memory
+    global cache
+    cache = Cache(NUM_SETS, ASSOCIATIVITY, CACHE_BLOCK_SIZE)
+    for i in range(MEM_SIZE // 4):
+        word_to_bytes(memory, 4*i, 4*i, WORDLENGTH)
+    print(memory)
 
 
 #TODO fix return statement - think it's a problem with the way memory is initialized but not entirely sure?
@@ -73,31 +73,31 @@ def readWord(address):
 
     # check if tag is in use in set of blocks - read hit
     for i in range(ASSOCIATIVITY):
-        if sysCache.sets[index].blocks[i].tag == tag:
+        if cache.sets[index].blocks[i].tag == tag:
             # update tag queue
             print("read hit: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
     
     # check if there are any empty blocks in the set - read miss
     for m in range(ASSOCIATIVITY):
-        if sysCache.sets[index].blocks[m].valid == False:
+        if cache.sets[index].blocks[m].valid == False:
             # update tag queue
-            sysCache.sets[index].blocks[m].tag = tag
-            sysCache.sets[index].blocks[m].valid = True
+            cache.sets[index].blocks[m].tag = tag
+            cache.sets[index].blocks[m].valid = True
             rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
             for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
-                sysCache.sets[index].blocks[m].data.append(memory[j])
+                cache.sets[index].blocks[m].data.append(memory[j])
             print("read miss: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
             return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
     
     # replace least recently used block - read miss + replace
     for n in range(ASSOCIATIVITY): # update for part 2
         # find least recently used block and update tag queue
-        sysCache.sets[index].blocks[i].tag = tag
-        sysCache.sets[index].blocks[i].valid = True
+        cache.sets[index].blocks[i].tag = tag
+        cache.sets[index].blocks[i].valid = True
         rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
         for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
-            sysCache.sets[index].blocks[i].data.append(memory[j])
+            cache.sets[index].blocks[i].data.append(memory[j])
         print("read miss + replace: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " new range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
         return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
 
@@ -109,6 +109,22 @@ def writeWord(address, word):
 def checkAllignment(address):
     assert address + 3 < MEM_SIZE
     assert address % 4 == 0
+
+
+def word_to_bytes(destination, start, word, size):
+    for i in range(size):
+        v = word % 256
+        destination[i+start] = v
+        word = word // 256
+
+
+def bytes_to_word(source, start, size):
+    word = 0
+    mult = 1
+    for i in range(size):
+        word = word + mult * source[start+i]
+        mult = mult * 256
+    return word
 
 
 main()
