dfbed4e6df85364faa1ce2b345bff515be7bc23b
jjklug <jackklug13@gmail.com>
2024-02-28 17:11:43 -0500

    check alignment

--- cachesim.py
+++ cachesim.py
@@ -8,21 +8,21 @@ NUM_SETS = CACHE_SIZE // (CACHE_BLOCK_SIZE * ASSOCIATIVITY)
 NUM_BLOCKS = CACHE_SIZE // CACHE_BLOCK_SIZE
 
 
-# helper funtions
+# helper fun ctions
 def logb2(val):
-        i = 0
-        assert val > 0
-        while val > 0:
-            i = i + 1
-            val = val >> 1
-        return i - 1
+    i = 0
+    assert val > 0
+    while val > 0:
+        i = i + 1
+        val = val >> 1
+    return i - 1
 
 
 # data structures
 class CacheBlock:
     def __init__(self, cache_block_size):
         self.tag = -1
-        self.dirty = False # not needed for Part One
+        self.dirty = False  # not needed for Part One
         self.valid = False
         self.data = bytearray(cache_block_size)
 
@@ -30,12 +30,12 @@ class CacheBlock:
 class CacheSet:
     def __init__(self, cache_block_size, associativity):
         self.blocks = [CacheBlock(cache_block_size) for i in range(associativity)]
-        self.tag_queue = [-1 for i in range(associativity)] # not needed for Part One
+        self.tag_queue = [-1 for i in range(associativity)]  # not needed for Part One
 
 
 class Cache:
     def __init__(self, num_sets, associativity, cache_block_size):
-        self.write_through = False # not needed for Part One
+        self.write_through = False  # not needed for Part One
         self.sets = [CacheSet(cache_block_size, associativity) for i in range(num_sets)]
         memory_size_bits = logb2(MEM_SIZE)
         self.cache_size_bits = logb2(CACHE_SIZE)
@@ -55,11 +55,10 @@ def main():
     global cache
     cache = Cache(NUM_SETS, ASSOCIATIVITY, CACHE_BLOCK_SIZE)
     for i in range(MEM_SIZE // 4):
-        word_to_bytes(memory, 4*i, 4*i, WORDLENGTH)
-    print(memory)
+        word_to_bytes(memory, 4 * i, 4 * i, WORDLENGTH)
 
 
-#TODO fix return statement - think it's a problem with the way memory is initialized but not entirely sure?
+# TODO fix return statement - think it's a problem with the way memory is initialized but not entirely sure?
 def readWord(address):
     # validate address
     checkAllignment(address)
@@ -67,17 +66,18 @@ def readWord(address):
     # calculate tag, index, and offset
     indexSize = logb2(NUM_SETS)
     offsetSize = logb2(CACHE_BLOCK_SIZE)
-    tag = (address >> (offsetSize + indexSize)) & (2**(NUM_SETS - offsetSize - indexSize) - 1)
-    index = (address >> offsetSize) & (2**indexSize - 1)
-    offset = address & (2**offsetSize - 1)
+    tag = (address >> (offsetSize + indexSize)) & (2 ** (NUM_SETS - offsetSize - indexSize) - 1)
+    index = (address >> offsetSize) & (2 ** indexSize - 1)
+    offset = address & (2 ** offsetSize - 1)
 
     # check if tag is in use in set of blocks - read hit
     for i in range(ASSOCIATIVITY):
         if cache.sets[index].blocks[i].tag == tag:
-            # update tag queue
-            print("read hit: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset)
-            return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
-    
+            print("read hit: adress = ", address, " index: ", index, " block index: ", i, " tag: ", tag, " offset: ",
+                  offset)
+            return memory[address] + 256 * memory[address + 1] + 256 ** 2 * memory[address + 2] + 256 ** 3 * memory[
+                address + 3]
+
     # check if there are any empty blocks in the set - read miss
     for m in range(ASSOCIATIVITY):
         if cache.sets[index].blocks[m].valid == False:
@@ -87,24 +87,58 @@ def readWord(address):
             rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
             for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
                 cache.sets[index].blocks[m].data.append(memory[j])
-            print("read miss: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
-            return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
-    
+            print("read miss: adress = ", address, " index: ", index, " block index: ", i, " tag: ", tag, " offset: ",
+                  offset, " range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
+            return memory[address] + 256 * memory[address + 1] + 256 ** 2 * memory[address + 2] + 256 ** 3 * memory[
+                address + 3]
+
     # replace least recently used block - read miss + replace
-    for n in range(ASSOCIATIVITY): # update for part 2
+    for n in range(ASSOCIATIVITY):  # update for part 2
         # find least recently used block and update tag queue
         cache.sets[index].blocks[i].tag = tag
         cache.sets[index].blocks[i].valid = True
         rangeStart = CACHE_BLOCK_SIZE * (address // CACHE_BLOCK_SIZE)
         for j in range(rangeStart, rangeStart + CACHE_BLOCK_SIZE):
             cache.sets[index].blocks[i].data.append(memory[j])
-        print("read miss + replace: adress = ", address, " index: ", index, " block index: ", i," tag: ", tag, " offset: ", offset, " new range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
-        return memory[address] + 256*memory[address + 1] + 256**2*memory[address + 2] + 256**3*memory[address + 3]
+        print("read miss + replace: adress = ", address, " index: ", index, " block index: ", i, " tag: ", tag,
+              " offset: ", offset, " new range: ", rangeStart, "-", rangeStart + CACHE_BLOCK_SIZE - 1)
+        return memory[address] + 256 * memory[address + 1] + 256 ** 2 * memory[address + 2] + 256 ** 3 * memory[
+            address + 3]
 
 
 def writeWord(address, word):
     pass
+    # validate address
+    checkAllignment(address)
 
+    # calculate tag, index, and offset
+    indexSize = logb2(NUM_SETS)
+    offsetSize = logb2(CACHE_BLOCK_SIZE)
+    tag = (address >> (offsetSize + indexSize)) & (2 ** (NUM_SETS - offsetSize - indexSize) - 1)
+    index = (address >> offsetSize) & (2 ** indexSize - 1)
+    offset = address & (2 ** offsetSize - 1)
+
+    # check if tag is in use in set of blocks - read hit
+    for i in range(ASSOCIATIVITY):
+        if cache.sets[index].blocks[i].tag == tag:
+            # update tag queue - make sure it doesn't overfill
+            if cache.sets[index].tag_queue[0] == -1:
+                cache.sets[index].tag_queue[0] = tag
+            else:
+                for x in range(ASSOCIATIVITY - 2, -1, -1):
+                    cache.sets[index].tag_queue[x + 1] = cache.sets[index].tag_queue[x]
+                cache.sets[index].tag_queue[0] = tag
+
+            # set dirty flag to true
+            cache.sets[index].blocks[i].dirty = True
+
+            # print and write given word
+            print("read hit: address = ", address, " index: ", index, " block index: ", i, " tag: ", tag, " offset: ",
+                  offset)
+            cache.sets[index].blocks[i].data = (
+                    memory[address] + 256 * memory[address + 1] + 256 ** 2 * memory[address + 2] + 256 ** 3 *
+                    memory[
+                        address + 3])
 
 def checkAllignment(address):
     assert address + 3 < MEM_SIZE
@@ -114,7 +148,7 @@ def checkAllignment(address):
 def word_to_bytes(destination, start, word, size):
     for i in range(size):
         v = word % 256
-        destination[i+start] = v
+        destination[i + start] = v
         word = word // 256
 
 
@@ -122,9 +156,10 @@ def bytes_to_word(source, start, size):
     word = 0
     mult = 1
     for i in range(size):
-        word = word + mult * source[start+i]
+        word = word + mult * source[start + i]
         mult = mult * 256
     return word
+
 
 
 main()
